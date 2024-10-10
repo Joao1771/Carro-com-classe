8bc359da2f869bcf3f4c7d5991b367af53dfc139
João Flávio <joaoflaviog1@gmail.com>
2024-10-10 12:28:43 -0300

    speedometer and buttons start
    velocímetro terminado e começando a por os botões de acelerar, frear e brecar

--- assets/script.js
+++ assets/script.js
@@ -8,33 +8,33 @@ class Carro {
     }
     /* Essas 3 funções vão apenas alterar o valor de v (velocidade)*/
     acelerar() {
-        if (this.v > 119) return
+        if (this.v > 169) return
         this.v++
         this.update()
     }
     frear() {
-        if (this.v > 119) return
+        if (this.v <= 0) return
         this.v--
         this.update()
     }
     brecar() {
-        if (this.v > 119) return
         this.v = 0
         this.update()
     }
     /* Onde vai ter os eventListener e vai rodar as outras class.functions()*/
     velocidade() {
-        document.addEventListener('keypress', (e) => {
-            if (e.key == 'd' || e.key == 'w') {
-                this.acelerar()
-            }
-            if (e.key == 'a' || e.key == 's') {
-                this.frear()
-            }
-            if (e.key == ' ') {
-                this.brecar()
-            }
+        const botoes = document.querySelector('.botoes')
+        document.addEventListener('keydown', (e) => {
+            if (e.key == 'd' || e.key == 'w' || e.key == 'ArrowUp' || e.key == 'ArrowRight') this.acelerar()
+            if (e.key == 'a' || e.key == 's' || e.key == 'ArrowLeft' || e.key == 'ArrowDown') this.frear()
+            if (e.key == ' ') this.brecar()
         })
+        botoes.addEventListener('click', (e) => {
+            if(e.target == botoes.querySelector('.acelerador')) this.acelerar()
+            if(e.target == botoes.querySelector('.freio')) this.frear()
+            if(e.target == botoes.querySelector('.freio-mao')) this.brecar()
+        })
+    this.update()
     }
     /* Define os valores das animações do fundo, da rua, etc. */
     update() {
@@ -45,7 +45,15 @@ class Carro {
         rodas[0].querySelector('img').style.animationDuration = this.msvalue(4820, true)
         rodas[1].querySelector('img').style.animationDuration = this.msvalue(4820, true)
         cenario.style.animationDuration = this.msvalue(25100)
+        this.euShowSpeed()
         console.log(this.v)
+    }
+    // controla o velocimetro e seu ponteiro (muito bom)
+    euShowSpeed(){
+        const ponteiro = document.querySelector('.ponteiro')
+        const p = document.querySelector('.vel-show').querySelector('p')
+        ponteiro.style.rotate = 248 + this.v + 'deg'
+        p.innerText = this.v + 'Km/h'
     }
     /* retorna o valor da duração em ms sem possibilade de ser 0ms */
     msvalue(inicial = 0, roda = false) {
