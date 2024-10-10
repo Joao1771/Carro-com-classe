8a569b74289a448be57529672f345a4811236a4e
João Flávio <joaoflaviog1@gmail.com>
2024-10-10 17:40:14 -0300

    buttons completed
    botões terminados e freio de mão adicionado

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,11 @@ class Carro {
     /* v = velocidade */
     constructor(v) {
         this.v = v
-    }
-    fala() {
-        console.log(this.v)
+        this.fDeMão = false
     }
     /* Essas 3 funções vão apenas alterar o valor de v (velocidade)*/
     acelerar() {
-        if (this.v > 169) return
+        if (this.fDeMão === true || this.v > 169) return
         this.v++
         this.update()
     }
@@ -18,12 +16,15 @@ class Carro {
         this.update()
     }
     brecar() {
+        if(this.fDeMão) this.fDeMão = false
+        else this.fDeMão = true
         this.v = 0
         this.update()
     }
     /* Onde vai ter os eventListener e vai rodar as outras class.functions()*/
     velocidade() {
         const botoes = document.querySelector('.botoes')
+        const brek = botoes.querySelector('.freio-mao')
         document.addEventListener('keydown', (e) => {
             if (e.key == 'd' || e.key == 'w' || e.key == 'ArrowUp' || e.key == 'ArrowRight') this.acelerar()
             if (e.key == 'a' || e.key == 's' || e.key == 'ArrowLeft' || e.key == 'ArrowDown') this.frear()
@@ -32,7 +33,7 @@ class Carro {
         botoes.addEventListener('click', (e) => {
             if(e.target == botoes.querySelector('.acelerador')) this.acelerar()
             if(e.target == botoes.querySelector('.freio')) this.frear()
-            if(e.target == botoes.querySelector('.freio-mao')) this.brecar()
+            if(e.target == brek || e.target == brek.querySelector('div') || e.target == brek.querySelectorAll('div')[1]) this.brecar()
         })
     this.update()
     }
@@ -46,6 +47,7 @@ class Carro {
         rodas[1].querySelector('img').style.animationDuration = this.msvalue(4820, true)
         cenario.style.animationDuration = this.msvalue(25100)
         this.euShowSpeed()
+        this.puxado()
         console.log(this.v)
     }
     // controla o velocimetro e seu ponteiro (muito bom)
@@ -54,6 +56,17 @@ class Carro {
         const p = document.querySelector('.vel-show').querySelector('p')
         ponteiro.style.rotate = 248 + this.v + 'deg'
         p.innerText = this.v + 'Km/h'
+    }
+    // mostra se o freio de mão está puxado ou não
+    puxado(){
+        const pn = document.querySelector('.freio-mao').querySelectorAll('div')[0]
+        if(this.fDeMão){
+            pn.style.rotate = '320deg'
+            pn.style.top = '25px'
+        } else {
+            pn.style.rotate = '290deg'
+            pn.style.top = '30px'
+        }
     }
     /* retorna o valor da duração em ms sem possibilade de ser 0ms */
     msvalue(inicial = 0, roda = false) {
